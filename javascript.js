e6716289ae4792723727528ab1e91bb78d529023
Hamad Alhibshi <hamad.alhibshi@gmail.com>
2023-07-12 15:03:50 +0300

    with for loops for questions

--- javascript.js
+++ javascript.js
@@ -7,72 +7,113 @@ const cat5 = $('.cat5qs');
 
 
 points.forEach((el) => {
-    cat1.append(`<button class='questionsButton'>${el}</button>`);
-    cat2.append(`<button class='questionsButton'>${el}</button>`);
-    cat3.append(`<button class='questionsButton'>${el}</button>`);
-    cat4.append(`<button class='questionsButton'>${el}</button>`);
-    cat5.append(`<button class='questionsButton'>${el}</button>`);
+    cat1.append(`<button class='questionsButton' id ='0'>${el}</button>`);
+    cat2.append(`<button class='questionsButton' id ='1'>${el}</button>`);
+    cat3.append(`<button class='questionsButton' id ='2'>${el}</button>`);
+    cat4.append(`<button class='questionsButton' id ='3'>${el}</button>`);
+    cat5.append(`<button class='questionsButton' id ='4'>${el}</button>`);
 });
 
 const questionsButtons = document.querySelectorAll(".questionsButton");
 const answerOptions = document.querySelectorAll(".answerOptions");
 const dialog = document.querySelector(".dialogOpen");
 
+let elPoints
+let randomInt
+let catIndex
+let minInt = 0
+let maxInt = 4
+
+function questionsLength() {
+    return getCategory[catIndex].questions.length
+}
+
+// Random Int Generator
+function getRandomInt(minInt, maxInt) {
+        minInt = Math.ceil(minInt);
+        maxInt = Math.floor(maxInt);
+        return Math.floor(Math.random() * (maxInt - minInt)) + minInt;
+    }
+
+
 questionsButtons.forEach((el) => {
-    el.addEventListener('click', () => {
-
-
-    // TODO Find which category was clicked
+    el.addEventListener('click', (e) => {
 
         cat1.click(() => {
-            $('#dialogHeader').text(getCategory[0].questions[0].question)
+            catIndex = 0;
+            maxInt = questionsLength();
+            console.log("MAX-INT:", maxInt);
+            randomInt = getRandomInt(minInt, maxInt);
+            console.log("RANDOM-INT:", randomInt);
 
+            $('#dialogHeader').text(getCategory[0].questions[randomInt].question);
+            
             for (let i = 0; i < 3; i++) {
-                $(`.answer${i+1}`).text(getCategory[0].questions[0].answers[i].answer)                
+                $(`.answer${i+1}`).attr('data-questionsAtt',randomInt);
+                $(`.answer${i+1}`).text(getCategory[0].questions[randomInt].answers[i].answer);           
             }
         });
 
         cat2.click(() => {
-            $('#dialogHeader').text(getCategory[1].questions[0].question)
+            catIndex = 1;
+            maxInt = questionsLength();
+            randomInt = getRandomInt(minInt, maxInt);
+
+            $('#dialogHeader').text(getCategory[1].questions[randomInt].question);
 
             for (let i = 0; i < 3; i++) {
-                $(`.answer${i+1}`).text(getCategory[1].questions[0].answers[i].answer)                
+                $(`.answer${i+1}`).attr('data-questionsAtt',randomInt);
+                $(`.answer${i+1}`).text(getCategory[1].questions[randomInt].answers[i].answer);              
             }
         }); 
 
         cat3.click(() => {
-            $('#dialogHeader').text(getCategory[2].questions[0].question)
+            catIndex = 2;
+            maxInt = questionsLength();
+            randomInt = getRandomInt(minInt, maxInt);
+
+            $('#dialogHeader').text(getCategory[2].questions[randomInt].question);
 
             for (let i = 0; i < 3; i++) {
-                $(`.answer${i+1}`).text(getCategory[2].questions[0].answers[i].answer)                
+                $(`.answer${i+1}`).attr('data-questionsAtt',randomInt);
+                $(`.answer${i+1}`).text(getCategory[2].questions[randomInt].answers[i].answer);              
             }
         });
 
         cat4.click(() => {
-            $('#dialogHeader').text(getCategory[3].questions[0].question)
+            catIndex = 3;
+            maxInt = questionsLength();
+            randomInt = getRandomInt(minInt, maxInt);
+
+            $('#dialogHeader').text(getCategory[3].questions[randomInt].question);
 
             for (let i = 0; i < 3; i++) {
-                $(`.answer${i+1}`).text(getCategory[3].questions[0].answers[i].answer)                
+                $(`.answer${i+1}`).attr('data-questionsAtt',randomInt);
+                $(`.answer${i+1}`).text(getCategory[3].questions[randomInt].answers[i].answer);            
             }
         });
 
         cat5.click(() => {
-            $('#dialogHeader').text(getCategory[4].questions[0].question)
+            catIndex = 4;
+            maxInt = questionsLength();
+            randomInt = getRandomInt(minInt, maxInt);
+
+            $('#dialogHeader').text(getCategory[4].questions[randomInt].question);
 
             for (let i = 0; i < 3; i++) {
-                $(`.answer${i+1}`).text(getCategory[4].questions[0].answers[i].answer)                
+                $(`.answer${i+1}`).attr('data-questionsAtt',randomInt);
+                $(`.answer${i+1}`).text(getCategory[4].questions[randomInt].answers[i].answer);               
             }
         });
 
     // Find which value was clicked
 
-        let buttonClickPoints = el.innerText;
-        console.log(buttonClickPoints);
-
+        elPoints = e.target.innerText;
+        // console.log(elPoints)
     // `Get rid of the loop
-
-
         dialog.show();
+
+        return elPoints, randomInt, catIndex, maxInt;
     });
 })
 
@@ -81,34 +122,60 @@ const dialogWrong = document.querySelector('.dialogWrong');
 const closeButton1 = document.querySelector('.closeButton1');
 const closeButton2 = document.querySelector('.closeButton2');
 
+let count = 1;
+let score1 = 0;
+let score2 = 0;
+
+const score1El = $('.score1');
+const score2El = $('.score2');
 
 answerOptions.forEach((el) => {
     el.addEventListener('click', (e) => {
+        function removeQuestion() {
+            let updatedCategory = getCategory;
+
+            updatedCategory[catIndex].questions.splice(randomInt, 1);
+            localStorage.setItem("category", JSON.stringify(updatedCategory));
+
+            console.log(updatedCategory)
+        }
+
         for (let i = 0; i < getCategory.length; i++) {
+            
             for (let j = 0; j < getCategory[i].questions.length; j++) {
+
                 for (let k = 0; k < getCategory[i].questions[j].answers.length; k++) { 
-                    // const selectedOption = e.target.innerText;
-                    // const answer = getCategory[i].questions[j].answers[k].answer;
-                    // const isCorrect = getCategory[i].questions[j].answers[k].isCorrect;
+            
+                    const responseId = parseInt(e.target.id);
+                    const questionIndex = parseInt(e.target.dataset.questionsatt);
+                    const isCorrect = getCategory[i].questions[questionIndex].answers[responseId].isCorrect;
 
-                    // console.log(`selectedOption ==> ${selectedOption}`);
-                    // console.log(`answer ==> ${answer}`);
-                    // console.log(`isCorrect ==> ${isCorrect}`);
-                    // console.log(` `);
+                    console.log(`isCorrect ${isCorrect}`);
+                    console.log(`option choice ${responseId}`);
+                    console.log(`questionIndex ${questionIndex}`);
+                    console.log(`category ${i}`);
 
-                    // Get id of element that was clicked and use it to get the isCorrect
-                    const answerButtonId = e.target.id; // convert this to a number
-                    const answerButtonIdNumber = parseInt(answerButtonId);
-                    const isCorrectCheck = getCategory[i].questions[j].answers[answerButtonId].isCorrect;
-                    console.log(`ID: ${answerButtonId}`);
-                    console.log(`NO. ID ${answerButtonIdNumber}`);
-                    console.log(`isCorrect: ${isCorrectCheck}`);
+                    // SCORE
+                    if(isCorrect) {
+                        if(count % 2 === 0){
+                            score2 += parseInt(elPoints);
+                            let score2Str = score2.toString();
+                            console.log(score2Str);
+                            score2El.text(score2Str);
+                        }
 
-                    const selectedEl = getCategory[i].questions[j].answers[answerButtonId]
-                    //check if selectedEl is correct
-                    // selectedEl.isCorrect
+                        else {
+                            score1 += parseInt(elPoints);
+                            let score1Str = score1.toString();
+                            console.log(elPoints);
+                            score1El.text(score1Str);
+                        }
+                    }
+                    else {
+                        count++;
+                    }
 
-                    if (selectedEl.isCorrect) {
+                    if (isCorrect) {
                         dialog.close();
                         dialogCorrect.show();
 
@@ -116,11 +183,15 @@ answerOptions.forEach((el) => {
                             dialogCorrect.close();
                         })
 
-                        console.log(e.target.innerText)
-                        console.log(true)
-                        return true
+                        console.log(e.target.innerText);
+                        console.log(true);
+                        removeQuestion();
+
+                        return true;
                     }
                     else {
+                        // const returnRightAnswer = document.querySelector(".returnCorrectAnswer")
+                        // returnRightAnswer.innerHTML()
                         dialog.close();
                         dialogWrong.show();
 
@@ -128,22 +199,25 @@ answerOptions.forEach((el) => {
                             dialogWrong.close();
                         });
 
-                        console.log(e.target.innerText)
-                        getCategory[i].questions[j].answers[k].answer
-                        console.log(false)
-                        return false
+                        console.log(e.target.innerText);
+                        getCategory[i].questions[j].answers[k].answer;
+                        removeQuestion();
+
+                        console.log(false);
+                        return false;
                     }
                 }
             }
         }
-        console.log(e.target.innerText)
+        console.log(e.target.innerText);
+
     });
 })
 
 const category = [
     {questions: [
         {question: 'Will Andrew Tate ever be a free man again?',
-        answers: [{answer: 'The Top G is always a free man', isCorrect: true}, {answer: 'The matrix got him', isCorrect: false}, {answer: 'Never', isCorrect: false}]},
+        answers: [{answer: 'The matrix got him', isCorrect: false}, {answer: 'The Top G is always a free man', isCorrect: true}, {answer: 'Never', isCorrect: false}]},
 
         {question: "What was Andrew Tate's comments about vaping?",
         answers: [{answer: 'Breathe Air', isCorrect: true}, {answer: "It's cool", isCorrect: false}, {answer: 'Just smoke cigars', isCorrect: false}]},
@@ -152,10 +226,10 @@ const category = [
         answers: [{answer: 'Copper', isCorrect: true}, {answer: 'Silver', isCorrect: false}, {answer: 'Blue', isCorrect: false}]},
 
         {question: "What sport did Andrew Tate used to play?", 
-        answers: [{answer: 'Kickboxing', isCorrect: true}, {answer: 'Boxing', isCorrect: false}, {answer: 'MMA', isCorrect: false}]},
+        answers: [{answer: 'Boxing', isCorrect: false}, {answer: 'MMA', isCorrect: false}, {answer: 'Kickboxing', isCorrect: true}]},
 
         {question: "What is the name of Andrew Tate's brother?", 
-        answers: [{answer: 'Tristan', isCorrect: true}, {answer: 'Alejandro', isCorrect: false}, {answer: 'Michael', isCorrect: false}]},
+        answers: [{answer: 'Alejandro', isCorrect: false}, {answer: 'Tristan', isCorrect: true}, {answer: 'Michael', isCorrect: false}]},
     ]},
 
     {questions: [
@@ -163,21 +237,21 @@ const category = [
         answers: [{answer: 'Itachi Uchiha', isCorrect: true}, {answer: 'Kakashi Hatake', isCorrect: false}, {answer: 'Jiraiya', isCorrect: false}]},
 
         {question: 'Who is the captain of the Straw Hat Pirates in the manga series One Piece?', 
-        answers: [{answer: 'Monkey D. Luffy', isCorrect: true}, {answer: 'Roronoa Zoro', isCorrect: false}, {answer: 'Trafalgar Law', isCorrect: false}]},
+        answers: [{answer: 'Roronoa Zoro', isCorrect: false}, {answer: 'Trafalgar Law', isCorrect: false}, {answer: 'Monkey D. Luffy', isCorrect: true}]},
 
         {question: 'Who is the main protagonist of the anime/manga series "Demon Slayer: Kimetsu no Yaiba"?', 
         answers: [{answer: 'Tanjiro Kamado)', isCorrect: true}, {answer: 'Zenitsu Agatsuma', isCorrect: false}, {answer: 'Inosuke Hashibira', isCorrect: false}]},
 
         {question: 'What is the name of the main character in the anime/manga series "Bleach"?', 
-        answers: [{answer: 'Ichigo Kurosaki', isCorrect: true}, {answer: 'Izuru Kira', isCorrect: false}, {answer: 'Rukia Kuchiki', isCorrect: false}]},
+        answers: [{answer: 'Izuru Kira', isCorrect: false}, {answer: 'Ichigo Kurosaki', isCorrect: true}, {answer: 'Rukia Kuchiki', isCorrect: false}]},
 
         {question: 'Who is the main protagonist in Attack on Titan?', 
-        answers: [{answer: 'Eren Yeager', isCorrect: true}, {answer: 'Eren Yeager', isCorrect: false}, {answer: 'Mikasa Ackerman', isCorrect: false}]},
+        answers: [{answer: 'Eren Yeager', isCorrect: true}, {answer: 'Levi Ackerman', isCorrect: false}, {answer: 'Mikasa Ackerman', isCorrect: false}]},
     ]},
 
     {questions: [
         {question: 'Which superhero wields a mystical hammer known as Mjolnir?', 
-        answers: [{answer: 'Mr. Tor himself', isCorrect: true}, {answer: 'Iron Man', isCorrect: false}, {answer: 'Captain America', isCorrect: false}]},
+        answers: [{answer: 'Iron Man', isCorrect: false}, {answer: 'Captain America', isCorrect: false}, {answer: 'Mr. Tor himself', isCorrect: true}]},
 
         {question: 'Who is the alter ego of the superhero Iron Man?', 
         answers: [{answer: 'Tony Stark', isCorrect: true}, {answer: 'Bruce Wayne', isCorrect: false}, {answer: 'Peter Parker', isCorrect: false}]},
@@ -186,7 +260,7 @@ const category = [
         answers: [{answer: 'Black Panther', isCorrect: true}, {answer: 'Captain America', isCorrect: false}, {answer: 'Black Widow', isCorrect: false}]},
 
         {question: 'Which superhero has the ability to shrink in size while increasing in strength?', 
-        answers: [{answer: 'Ant-Man', isCorrect: true}, {answer: 'Hulk', isCorrect: false}, {answer: 'Spider-Man', isCorrect: false}]},
+        answers: [{answer: 'Hulk', isCorrect: false}, {answer: 'Ant-Man', isCorrect: true}, {answer: 'Spider-Man', isCorrect: false}]},
 
         {question: 'Which Marvel superhero is known for his incredible strength, green skin, and uncontrollable rage?', 
         answers: [{answer: 'Hulk', isCorrect: true}, {answer: 'Thor', isCorrect: false}, {answer: 'Iron Man', isCorrect: false}]},
@@ -198,36 +272,36 @@ const category = [
         answers: [{answer: 'A team-based first-person shooter', isCorrect: true}, {answer: 'A puzzle-solving adventure', isCorrect: false}, {answer: 'A competitive cooking game', isCorrect: false}]},
 
         {question: 'In the game "The Legend of Zelda: Breath of the Wild," what is the name of the main character?', 
-        answers: [{answer: 'Link', isCorrect: true}, {answer: 'Mario', isCorrect: false}, {answer: 'Link', isCorrect: false}]},
+        answers: [{answer: 'Link', isCorrect: true}, {answer: 'Mario', isCorrect: false}, {answer: 'Superman', isCorrect: false}]},
 
         {question: 'Which video game franchise features a protagonist named Nathan Drake?',
-        answers: [{answer: 'Uncharted', isCorrect: true}, {answer: "Assassin's Creed", isCorrect: false}, {answer: 'Call of Duty', isCorrect: false}]},
+        answers: [{answer: "Assassin's Creed", isCorrect: false}, {answer: 'Call of Duty', isCorrect: false}, {answer: 'Uncharted', isCorrect: true}]},
 
         {question: 'In the game "Super Mario Bros," what is the name of the main antagonist that Mario continuously battles against?',
         answers: [{answer: 'Bowser', isCorrect: true}, {answer: 'Wario', isCorrect: false}, {answer: 'Donkey Kong', isCorrect: false}]},
 
         {question: 'In the game "Minecraft," what is the primary goal or objective for players to accomplish?',
-        answers: [{answer: 'Survive and thrive in a randomly generated world', isCorrect: true}, {answer: 'Solve puzzles in a dungeon', isCorrect: false}, {answer: 'Survive and thrive in a randomly generated world b) Build and manage a city', isCorrect: false}]},
+        answers: [{answer: 'Solve puzzles in a dungeon', isCorrect: false}, {answer: 'Survive and thrive in a randomly generated world', isCorrect: true},{answer: 'Build and manage a city', isCorrect: false}]},
     ]},
 
     {questions: [
-        {question: 'Which band created the song Bohemian Rhapsody?',
-        answers: [{answer: 'Queen', isCorrect: true}, {answer: 'Twisted Sisters', isCorrect: false}, {answer: 'AC DC', isCorrect: false}]},
+        {question: 'What is banned in public places in Florida after 6pm on a Thursday?',
+        answers: [{answer: 'Cycling', isCorrect: false}, {answer: 'Farting', isCorrect: true}, {answer: 'Swimming', isCorrect: false}]},
 
-        {question: 'C5Q2',
-        answers: [{answer: 'Why', isCorrect: true}, {answer: 'A2', isCorrect: false}, {answer: 'A3', isCorrect: false}]},
+        {question: 'On Sunday, what is illegal to sell in Columbus, Ohio?',
+        answers: [{answer: 'Cornflakes', isCorrect: true}, {answer: 'Games', isCorrect: false}, {answer: 'Cars', isCorrect: false}]},
 
-        {question: 'C5Q3',
-        answers: [{answer: 'Where', isCorrect: true}, {answer: 'A2', isCorrect: false}, {answer: 'A3', isCorrect: false}]},
+        {question: 'Whats is Illegal in Alabama?',
+        answers: [{answer: 'Guns', isCorrect: false}, {answer: 'Driving Blindfolded', isCorrect: true}, {answer: 'Window tinting', isCorrect: false}]},
 
-        {question: 'C5Q4',
-        answers: [{answer: 'Nice', isCorrect: true}, {answer: 'A2', isCorrect: false}, {answer: 'A3', isCorrect: false}]},
+        {question: 'What is Illegal in Arizona?',
+        answers: [{answer: "You can't use a toaster at 10pm", isCorrect: false}, {answer: "Football", isCorrect: false}, {answer: "You can't have a donkey sleeping in your bathtub after 7pm", isCorrect: true}]},
 
-        {question: 'C5Q5',
-        answers: [{answer: 'Crazy', isCorrect: true}, {answer: 'A2', isCorrect: false}, {answer: 'A3', isCorrect: false}]},
+        {question: 'What is Illegal in Maine?',
+        answers: [{answer: 'To bite your landlord', isCorrect: true}, {answer: 'Running', isCorrect: false}, {answer: 'Netlix after 7pm', isCorrect: false}]},
     ]}
 ]
 
-localStorage.setItem('category', JSON.stringify(category))
-const getCategory = JSON.parse(localStorage.getItem('category'))
-console.log(getCategory)
+localStorage.setItem('category', JSON.stringify(category));
+const getCategory = JSON.parse(localStorage.getItem('category'));
+console.log(getCategory);
